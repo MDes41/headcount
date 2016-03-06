c5108638ff7e82f0ddada61ce638778b90c61f75
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-03-05 23:01:05 -0700

    proficiency by race working

--- lib/statewide_test_repository.rb
+++ lib/statewide_test_repository.rb
@@ -25,6 +25,18 @@ class StatewideTestRepository
     @repo[:statewide_testing][:eighth_grade]
   end
 
+  def math_proficiency_by_race
+    @repo[:statewide_testing][:math]
+  end
+
+  def reading_proficiency_by_race
+    @repo[:statewide_testing][:reading]
+  end
+
+  def writing_proficiency_by_race
+    @repo[:statewide_testing][:writing]
+  end
+
   def district_groups_3g
     third_grade_proficiency.group_by do |repo_row|
       repo_row[:location]
@@ -37,13 +49,34 @@ class StatewideTestRepository
     end
   end
 
+  def district_groups_by_race_for_math
+    math_proficiency_by_race.group_by do |repo_row|
+      repo_row[:location]
+    end
+  end
+
+  def district_groups_by_race_for_reading
+    reading_proficiency_by_race.group_by do |repo_row|
+      repo_row[:location]
+    end
+  end
+
+  def district_groups_by_race_for_writing
+    writing_proficiency_by_race.group_by do |repo_row|
+      repo_row[:location]
+    end
+  end
+
   def statewide_test_instances
     proficiency_data_3g ||= create_hash_of_math_reading_writing_per_year_3g
     proficiency_data_8g ||= create_hash_of_math_reading_writing_per_year_8g
+    proficiency_by_race ||= create_hash_with_all_subjects
+    require "pry"; binding.pry
     district_groups_3g.keys.map do |district|
       StatewideTest.new( { name: district,
             proficiency_by_year_3g: proficiency_data_3g[district],
-            proficiency_by_year_8g: proficiency_data_8g[district]
+            proficiency_by_year_8g: proficiency_data_8g[district],
+               proficiency_by_race: proficiency_by_race[district]
                       } )
     end
   end
@@ -81,5 +114,43 @@ class StatewideTestRepository
   def truncate(value)
     ((value.to_f * 1000).floor / 1000.0 )
   end
+
+  #create_hash_for_race_groups
+
+  def create_hash_with_all_subjects
+    @subject_proficiencies = {}
+    math = district_groups_by_race_for_math
+    reading = district_groups_by_race_for_reading
+    writing = district_groups_by_race_for_writing
+    create_hash_of_for_race_groups(math: math)
+    create_hash_of_for_race_groups(reading: reading)
+    create_hash_of_for_race_groups(writing: writing)
+  end
+
+  def create_hash_of_for_race_groups(subject)
+    subject.values.first.map do |district, data|
+      [district, get_proficiency_data(data, subject.keys.first)]
+    end.to_h
+  end
+
+  def get_proficiency_data(data, subject)
+    group_by_race(data).map do |race, data|
+      [ race, hash_out_by_years(data, subject) ]
+    end.to_h
+  end
+
+  def group_by_race(data)
+    data.group_by do |data|
+      data[:race_ethnicity]
+    end.to_h
+  end
+
+  def hash_out_by_years(data, subject)
+    group_by_years(data).map do |year, data|
+      @subject_proficiencies[subject] = ('%.3f' % truncate(data.first[:data])).to_f
+      [year , @subject_proficiencies]
+    end.to_h
+  end
+
 
 end
--- test/statewide_test_repository_test.rb
+++ test/statewide_test_repository_test.rb
@@ -65,6 +65,29 @@ class StatewideTestRespositoryTest < Minitest::Test
 
   end
 
+  def test_proficient_by_race_or_ethnicity_returns_a_hash_grouped_by_racereferencing_percentages_by_subject
+    str = StatewideTestRepository.new
+    str.load_data({
+      :statewide_testing => {
+              :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
+             :eighth_grade => "./data/8th grade students scoring proficient or above on the CSAP_TCAP.csv",
+                     :math => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv",
+                  :reading => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv",
+                  :writing => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"
+                            }
+                  })
+    statewide_test = str.find_by_name("ACADEMY 20")
+
+    # str.create_hash_of_for_race_groups_in_math
+    # str.create_hash_of_for_race_groups_in_reading
+    str.create_hash_with_all_subjects
+
+
+    # statewide_test.proficient_by_race_or_ethnicity(:asian)
+
+  end
+
+
 
 
 
